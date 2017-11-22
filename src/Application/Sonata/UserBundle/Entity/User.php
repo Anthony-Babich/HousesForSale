0decc91ba78691689ac5037be7533667e96bf45a
Anton <antosha.1998.ru@mail.ru>
2017-11-22 09:23:00 +0300

    Готовая админка для тестирования.

---
+++ src/Application/Sonata/UserBundle/Entity/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Application\Sonata\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int $id
+     */
+    protected $id;
+
+    /**
+     * Get id.
+     *
+     * @return int $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
