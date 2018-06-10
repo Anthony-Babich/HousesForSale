9a73b822196a846cb196b7be5b3248c73e1b7aaf
Anton <antosha.1998.ru@mail.ru>
2018-06-10 12:27:40 +0300

    UI fixed for production.

--- web/app.php
+++ web/app.php
@@ -6,6 +6,7 @@ require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../app/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', false);
+//$kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
